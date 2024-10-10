36086d6fd3f4dd5af7b0e946b5742dfd9f04693f
evan <evan.c.straub@gmail.com>
2024-10-10 11:34:38 -0700

    updated License to OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         ],
         classifiers=[
             "Programming Language :: Python :: 3",
-            "License-File: LICENSE.txt",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
             "Operating System :: OS Independent",
         ]
 )
